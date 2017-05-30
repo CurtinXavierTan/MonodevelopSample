2f2e5182552b20a32726c1eae4428c73ef48c1fa
XavierTanXY <xaviertanxy@192-168-1-8.tpgi.com.au>
2017-05-30 15:14:30 +0800

    Updated readme and added files
    Added sample code and screenshots for showing that I am using
    MonoDevelop IDE on Linux

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MonodevelopSample
-Sample project using Monodevelop on Linux
+Sprint 4 - 5 Task for CCP
+
+Student ID: 18249833
+Name: Xhien Yi Tan ( Xavier )
+
+This is repository for Sprint 4 - 5
+Task 4.1.1 : Gain experience using MonoDevelop IDE using a GTK# Project
+
+I have created a sample C# program with GTK# using MonoDevelop IDE to make sure I can code a simple GTK# project using MonoDevelop on Linux. I have provided two screenshots for this task as well, to show that I am using that IDE.
---
+++ SampleProject/SampleProject/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void ndler (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnVoteClicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("You clicked vote");
+	}
+
+	protected void onChooseOtherClicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("You clicked choose other");
+	}
+
+	protected void OnCancelClicked (object sender, EventArgs e)
+	{
+		Console.WriteLine ("You clicked cancel");
+	}
+}
